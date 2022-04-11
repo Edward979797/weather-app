0b1d693d780643918ecd18f647b6f3fb419fb0cc
Edward979797 <edwardceapcin@gmail.com>
2022-04-11 18:03:52 -0400

    Basic skeleton done, but API key still needs a few hours to validate

---
+++ script.js
@@ -0,0 +1,14 @@
+async function retrieveWeather(city='Toronto') {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=54c3377a3ca9e9069ecac550e7f3a0a5`);
+
+        const data = await response.json();
+
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
+
+retrieveWeather();
