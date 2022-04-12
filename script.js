2610194a799eb11e6db85c036e6902642bba8073
Edward979797 <edwardceapcin@gmail.com>
2022-04-11 23:31:45 -0400

    Minor styling done and API is successfully connected (data is logged to console)

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const search = document.querySelector('#search');
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', () => {
+    try {
+        retrieveWeather(search.value);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
 async function retrieveWeather(city='Toronto') {
     try {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=54c3377a3ca9e9069ecac550e7f3a0a5`);
